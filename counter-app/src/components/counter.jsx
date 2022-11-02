072bc9a6b00f841ecc3c5ce81aa7fb308fe9341f
cBombs <richie.ucsb@gmail.com>
2022-11-01 17:36:08 -0700

    x

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class  Counter extends Component {
-    state = {  } 
-    render() { 
-        return ();
-    }
+class Counter extends Component {
+  state = {
+    count: 1,
+  };
+
+  styles = {
+    fontSize: 50,
+    fontWeight: "bold",
+  };
+
+  render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+
+    return (
+      <div>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className={classes}>Increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
-export default ;
+
+export default Counter;
