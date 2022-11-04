94a21e9d1c723c8b5296abd64a34132fa8f9d239
cBombs <richie.ucsb@gmail.com>
2022-11-03 17:23:25 -0700

    progress

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,31 +2,29 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
+    tags: [],
   };
 
-  styles = {
-    fontSize: 50,
-    fontWeight: "bold",
-  };
-
-  render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
 
     return (
-      <div>
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
-        <button className={classes}>Increment</button>
-      </div>
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && "Please make a new tag! "}
+        {this.renderTags()}
+      </div>
+    );
   }
 }
 
