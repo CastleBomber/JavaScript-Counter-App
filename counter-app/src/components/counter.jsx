bfbabc57b4c89b6648ae512654af9f9b7c4ee47f
cBombs <richie.ucsb@gmail.com>
2022-11-03 17:48:06 -0700

    p

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: [],
+    tags: ["tag1"],
   };
 
   renderTags() {
@@ -18,13 +18,33 @@ class Counter extends Component {
     );
   }
 
+  handleIncrement() {
+    console.log("Increment Clicked", this.state.count);
+  }
+
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please make a new tag! "}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 }
 
